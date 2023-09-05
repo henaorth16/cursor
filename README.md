7503bc9e541033965de6693ea0a78392640269fb
henaorth16 <emyayehenok@gmail.com>
2023-09-05 23:43:57 +0300

    finished part

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # This project pushes my js skill to forward
-###
-just download and enjoy
+---
+
+#####let me know if you want to get the javascript file only,
+
+
+####i will create the elements inside javascript and then you will be able to reuse the js file with your own project.
+---
+### The other thing what i added to this project is that if you hit right click every where you will get default service of your browser(thats customized😁)
+######Thanks...
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const cur = document.querySelector(".cursor");
+// const cur = document.querySelector(".cursor");
+const ulEl = document.querySelector("ul");
 
 document.addEventListener("mousemove",(event)=>{
-console.log(event.screenX - (window.innerWidth / 2));
 
 document.documentElement.style.setProperty("--left", event.pageX + 1)
 document.documentElement.style.setProperty("--top", event.pageY +10)
@@ -9,6 +9,23 @@ document.documentElement.style.setProperty("--top", event.pageY +10)
 
 document.addEventListener('contextmenu', function(ev) {
     ev.preventDefault();
-    alert('success!');
+    ulEl.style.left = `${ev.clientX}px`
+    ulEl.style.top = `${ev.clientY}px`
+    ulEl.style.display = `block`;
+    var liEl = document.querySelectorAll("ul > li")
+
+    liEl.forEach(item=>{
+        item.addEventListener("mouseover",()=>{
+            item.classList.add("active")
+        })
+        item.addEventListener("mouseleave",()=>{
+           item.classList.remove("active")
+        })
+        
+    })
     return false;
 }, false);
+
+document.addEventListener("click", () =>{
+    ulEl.style.display = "none"
+})
